ef1dc838bbca51537db3b59d317e89bac5e82bab
Qimat Luo <qimatluo@westpharma.com>
2022-11-30 17:32:40 +0800

    Do something for each player

--- vanilla/src/main.ts
+++ vanilla/src/main.ts
@@ -1,34 +1,17 @@
 import { askBetween, askNonEmpty, askYesNo } from "./ask";
-import { putStrLn, reload } from "./side-effects";
+import { reload } from "./side-effects";
 import { curry2 } from "fp-ts-std/Function";
-import { constant as c, flow, pipe } from "fp-ts/function";
+import { flow, pipe } from "fp-ts/function";
 import * as A from "fp-ts/Array";
 import * as I from "fp-ts/Identity";
-import { randomInt } from "fp-ts/Random";
+import * as RA from "fp-ts/ReadonlyArray";
 import * as T from "fp-ts/Task";
-
-//
-// helpers
-//
-
-// get a random int between 1 and 5
-const random = T.fromIO(randomInt(1, 5));
-
-//
-// game
-//
 
 function gameLoop(name: readonly string[]): T.Task<void> {
   return pipe(
-    T.Do,
-    T.apS("secret", random),
-    T.apS("guess", askBetween(`Dear ${name}, please guess a number.`, 1, 5, 1)),
-    T.chain(({ secret, guess }) =>
-      guess === secret
-        ? putStrLn(`You guessed right, ${name}!`)
-        : putStrLn(`You guessed wrong, ${name}! The number was: ${secret}`)
-    ),
-    T.chain(c(askYesNo(`Do you want to continue, ${name}?`, "y"))),
+    name,
+    RA.map((x) => askYesNo(`Player ${x} do something.`)),
+    T.sequenceSeqArray,
     T.chain((b) => (b ? gameLoop(name) : T.of(undefined)))
   );
 }
