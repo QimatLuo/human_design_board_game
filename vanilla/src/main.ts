05a0479af22d7478b1179664bf113b05a9396649
Qimat Luo <qimatluo@westpharma.com>
2022-11-30 15:10:40 +0800

    Add askBetween()

--- vanilla/src/main.ts
+++ vanilla/src/main.ts
@@ -1,8 +1,12 @@
 import { getStrLn, putStrLn, reload } from "./side-effects";
-import { curry2, guard } from "fp-ts-std/Function";
+import { curry2, curry4, guard } from "fp-ts-std/Function";
+import * as n from "fp-ts/number";
 import * as s from "fp-ts/string";
 import { constant as c, flow, pipe } from "fp-ts/function";
+import * as I from "fp-ts/Identity";
 import * as O from "fp-ts/Option";
+import { between } from "fp-ts/Ord";
+import * as P from "fp-ts/Predicate";
 import { randomInt } from "fp-ts/Random";
 import * as T from "fp-ts/Task";
 
@@ -13,6 +17,32 @@ import * as T from "fp-ts/Task";
 // ask something and get the answer
 function ask(question: string, defaultInput?: string): T.Task<string> {
   return pipe(putStrLn(question, defaultInput), T.fromIO, T.chain(c(getStrLn)));
+}
+
+function askBetween(
+  question: string,
+  min: number,
+  max: number,
+  defaultInput: number
+): T.Task<number> {
+  const loop = flow(
+    c(curry4(askBetween)),
+    I.ap(question),
+    I.ap(min),
+    I.ap(max),
+    I.ap(defaultInput)
+  );
+  return pipe(
+    ask(`${question} (${min}-${max})`, `${defaultInput}`),
+    T.map(Number),
+    T.chain(
+      flow(
+        O.fromPredicate(P.not(isNaN)),
+        O.chain(O.fromPredicate(between(n.Ord)(min, max))),
+        O.match(loop, T.of)
+      )
+    )
+  );
 }
 
 // get a random int between 1 and 5
