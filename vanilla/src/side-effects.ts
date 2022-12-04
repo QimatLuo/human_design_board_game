abb0b421a9f758c0f42f23ea05f346d17c9b3911
Qimat Luo <qimatluo@westpharma.com>
2022-12-04 14:45:06 +0800

    Add showErrors

--- vanilla/src/side-effects.ts
+++ vanilla/src/side-effects.ts
@@ -1,9 +1,13 @@
 /* eslint functional/no-return-void:0, functional/no-expression-statement:0, functional/immutable-data:0, functional/functional-parameters:0, functional/no-return-void:0, @typescript-eslint/prefer-readonly-parameter-types:0 */
 
+import { join } from "fp-ts-std/ReadonlyArray";
 import { warn } from "fp-ts/Console";
 import * as IOO from "fp-ts/IOOption";
+import * as O from "fp-ts/Option";
+import * as RA from "fp-ts/ReadonlyArray";
 import * as T from "fp-ts/Task";
 import { flow, pipe } from "fp-ts/function";
+import * as t from "io-ts";
 
 export const getStrLn: T.Task<string> = () =>
   new Promise((resolve) => {
@@ -43,5 +47,43 @@ export const putStrLn = flow(
     ),
   T.fromIO
 );
+
+export function showErrors(es: t.Errors) {
+  const msg = pipe(
+    es,
+    RA.map((x) =>
+      pipe(
+        O.fromNullable(x.message),
+        O.getOrElse(() =>
+          pipe(
+            x.context,
+            RA.map(
+              (x) => `Invalid value ${x.actual} supplied to : ${x.type.name}`
+            ),
+            join(", ")
+          )
+        )
+      )
+    ),
+    join(", ")
+  );
+  return pipe(
+    IOO.Do,
+    IOO.bind("li", () =>
+      IOO.fromNullable(
+        document.querySelector<HTMLLIElement>("ol > li:last-child")
+      )
+    ),
+    IOO.match(
+      () => alert(msg),
+      ({ li }) => {
+        const span = document.createElement("span");
+        span.className = "error";
+        span.innerText = msg;
+        li.append(span);
+      }
+    )
+  );
+}
 
 export const reload = () => location.reload();
