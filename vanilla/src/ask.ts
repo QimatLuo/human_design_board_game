5470c855c7a6ce8938e761f3e69f59de638c20ca
Qimat Luo <qimatluo@westpharma.com>
2022-12-04 14:46:03 +0800

    Migrate ask

--- vanilla/src/ask.ts
+++ vanilla/src/ask.ts
@@ -1,14 +1,13 @@
-import { getStrLn, putStrLn } from "./side-effects";
-import { curry2, curry4, guard } from "fp-ts-std/Function";
+import { Int3_6, YorN } from "./BrandTypes";
+import { getStrLn, putStrLn, showErrors } from "./side-effects";
+import { curry2, curry4 } from "fp-ts-std/Function";
 import * as I from "fp-ts/Identity";
 import * as O from "fp-ts/Option";
-import { between } from "fp-ts/Ord";
-import * as P from "fp-ts/Predicate";
 import * as T from "fp-ts/Task";
 import * as TE from "fp-ts/TaskEither";
 import { constant as c, flow, pipe } from "fp-ts/function";
-import * as n from "fp-ts/number";
 import * as s from "fp-ts/string";
+import { NonEmptyString } from "io-ts-types";
 
 function ask(question: string, defaultInput?: string): T.Task<string> {
   return pipe(putStrLn(question, defaultInput), T.fromIO, T.chain(c(getStrLn)));
@@ -19,7 +18,7 @@ export function askBetween(
   min: number,
   max: number,
   defaultInput: number
-): TE.TaskEither<Error, number> {
+): TE.TaskEither<never, Int3_6> {
   const loop = flow(
     c(curry4(askBetween)),
     I.ap(question),
@@ -29,14 +28,8 @@ export function askBetween(
   );
   return pipe(
     ask(`${question} (${min}-${max})`, `${defaultInput}`),
-    T.map(Number),
-    T.chain(
-      flow(
-        O.fromPredicate(P.not(isNaN)),
-        O.chain(O.fromPredicate(between(n.Ord)(min, max))),
-        O.match(loop, TE.of)
-      )
-    )
+    T.map(Int3_6.decode),
+    TE.orElse(flow(showErrors, TE.fromIO, TE.chain(loop)))
   );
 }
 
@@ -48,23 +41,20 @@ export function askYesNo(
   const eq = curry2(s.Eq.equals);
   return pipe(
     ask(`${question} (y/n)`, defaultInput),
-    T.map(s.toLowerCase),
-    T.chain(
-      guard<string, TE.TaskEither<Error, boolean>>([
-        [eq("y"), c(TE.of(true))],
-        [eq("n"), c(TE.of(false))],
-      ])(loop)
-    )
+    T.map(YorN.decode),
+    TE.map(flow(O.fromPredicate(eq("y")), O.match(c(false), c(true)))),
+    TE.orElse(flow(showErrors, TE.fromIO, TE.chain(loop)))
   );
 }
 
 export function askNonEmpty(
   question: string,
   defaultInput?: string
-): TE.TaskEither<Error, string> {
+): TE.TaskEither<never, NonEmptyString> {
   const loop = flow(c(curry2(askNonEmpty)), I.ap(question), I.ap(defaultInput));
   return pipe(
     ask(question, defaultInput),
-    T.chain(flow(O.fromPredicate(P.not(s.isEmpty)), O.match(loop, TE.of)))
+    T.map(NonEmptyString.decode),
+    TE.orElse(flow(showErrors, TE.fromIO, TE.chain(loop)))
   );
 }
